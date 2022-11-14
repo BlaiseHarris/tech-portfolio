e4f33ebb235cfbd33ee178caa606c16f080f6b00
BlaiseHarris <94414385+BlaiseHarris@users.noreply.github.com>
2022-11-14 10:39:09 -0500

    Updating Profile name, address, and social links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Blaise Harris', // Title for your website.
+    tagline: 'Developer, happy, and awesome',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://BlaiseHarris.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -72,9 +72,7 @@ const siteConfig = {
     If you want to remove a social media icon, you can add the two // before the line
     or delete the line. */
     socialMedia: {
-        linkedin: 'https://www.linkedin.com/in/',
-        twitter: 'https://twitter.com/',
-        github: 'https://github.com/',
+        github: 'https://github.com/BlaiseHarris',
         //facebook: 'https://www.facebook.com/',
         //instagram: 'https://www.instagram.com/',
         //youtube: 'https://www.youtube.com/',
